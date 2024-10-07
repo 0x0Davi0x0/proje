b3f5236ca378c8785d19fc3b8b080c123c402076
Davi Kasmirski dos Santos <134454080+0x0Davi0x0@users.noreply.github.com>
2024-10-07 19:47:03 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const calculadora = require('./calculadora');
+console.log('Olá mundo!');
+console.log(calculadora.somar(1, 2)); //soma
+console.log(calculadora.subtrair(1, 2)); //subtração
+//console.log(calculadora.MinhaFuncaoSoma(1, 2));
